39465f8978f9f3716794d7cfdb7fbb7d7f23e179
xiaobo.sun <xiaobo.sun@qq.com>
2018-12-04 10:45:25 +0800

    Update SendConfig.php

--- src/wechat/command/SendConfig.php
+++ src/wechat/command/SendConfig.php
@@ -40,7 +40,7 @@ class SendConfig extends Command
     public function execute(Input $input, Output $output)
     {
         //获取默认配置文件
-        $content = file_get_contents(Env::get('root_path') . 'vendor/zz-studio/think-wechat/src/config.php');
+        $content = file_get_contents(Env::get('root_path') . 'vendor/zzstudio/think-wechat/src/config.php');
 
         $configPath = Env::get('config_path') . '/';
         $configFile = $configPath . 'wechat.php';
